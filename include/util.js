8fac2c2b972887770590845bac23192fe318d83d
Andrew Gu <mykonian@gmail.com>
2019-07-31 13:16:27 -0700

    Update code

--- include/util.js
+++ include/util.js
@@ -15,7 +15,7 @@ function initInteractiveFormatting(formatMapping)
     var expanderClass = formatMapping["expand_css_class"];
     var expanderAttrbString = formatMapping["expander_attrb"];
 
-    var expanderElements = document.querySelectorAll("input [" + expanderAttrbString + "] [type=checkbox]");
+    var expanderElements = document.querySelectorAll("input[" + expanderAttrbString + "][type=checkbox]");
     expanderElements.forEach(function(element, index)
     {
         var targetElemId = element.getAttribute(expanderAttrbString);
