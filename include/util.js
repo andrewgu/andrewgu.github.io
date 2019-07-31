a00d61a9f5428ed155eaf7a14e2f97af6049d2f1
Andrew Gu <mykonian@gmail.com>
2019-07-31 13:12:26 -0700

    Writing code

--- include/util.js
+++ include/util.js
@@ -1,4 +1,29 @@
+function updateExpandState(targetNode, expanderClass, expanded)
+{
+    if (expanded)
+    {
+        targetNode.classList.add(expanderClass);
+    }
+    else
+    {
+        targetNode.classList.remove(expanderClass);
+    }
+}
+
 function initInteractiveFormatting(formatMapping)
 {
-    
+    var expanderClass = formatMapping["expand_css_class"];
+    var expanderAttrbString = formatMapping["expander_attrb"];
+
+    var expanderElements = document.querySelectorAll("input [" + expanderAttrbString + "] [type=checkbox]");
+    expanderElements.forEach(function(element, index)
+    {
+        var targetElemId = element.getAttribute(expanderAttrbString);
+        // Register handler
+        element.onclick = function() {
+            updateExpandState(document.querySelector("#" + targetElemId), expanderClass, element.checked);
+        }
+        // Update current state
+        updateExpandState(document.querySelector("#" + targetElemId), expanderClass, element.checked);
+    });
 }
