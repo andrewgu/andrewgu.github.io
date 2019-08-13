629f7cb5b63f123961725fdb6a6f10635c62396e
Andrew Gu <mykonian@gmail.com>
2019-08-12 22:44:15 -0700

    work in progress

--- include/util.js
+++ include/util.js
@@ -1,29 +1,56 @@
-function updateExpandState(targetNode, expanderClass, expanded)
+function updateExpandState(targetNode, collapserClass, expanded)
 {
     if (expanded)
     {
-        targetNode.classList.add(expanderClass);
+        targetNode.classList.remove(collapserClass);
     }
     else
     {
-        targetNode.classList.remove(expanderClass);
+        targetNode.classList.add(collapserClass);
+    }
+}
+
+function flipExpandState(element, attrbName)
+{
+    var state = getExpandState(element, attrbName);
+    element.setAttribute(attrbName, state ? "false" : "true");
+}
+
+function getExpandState(element, attrbName)
+{
+    var attrbValue = element.getAttribute(attrbName);
+    if (attrbValue === null || attrbValue === "false")
+    {
+        return false;
+    }
+    else if (attrbValue === "true")
+    {
+        return true;
+    }
+    else
+    {
+        return !!attrbValue;
     }
 }
 
 function initInteractiveFormatting(formatMapping)
 {
-    var expanderClass = formatMapping["expand_css_class"];
+    // Use "collapse" CSS class as the modified setting so that no-JS fails to open
+    var collapserClass = formatMapping["collapse_css_class"];
     var expanderAttrbString = formatMapping["expander_attrb"];
+    var expandStateAttrb = formatMapping["expand_state_attrb"];
 
-    var expanderElements = document.querySelectorAll("input[" + expanderAttrbString + "][type=checkbox]");
+    var expanderElements = document.querySelectorAll("input[" + expanderAttrbString + "]");
     expanderElements.forEach(function(element, index)
     {
         var targetElemId = element.getAttribute(expanderAttrbString);
         // Register handler
         element.onclick = function() {
-            updateExpandState(document.querySelector("#" + targetElemId), expanderClass, element.checked);
+            flipExpandState(element, expandStateAttrb);
+            updateExpandState(document.querySelector("#" + targetElemId), collapserClass, getExpandState(element, expandStateAttrb));
+            
         }
         // Update current state
-        updateExpandState(document.querySelector("#" + targetElemId), expanderClass, element.checked);
+        updateExpandState(document.querySelector("#" + targetElemId), collapserClass, getExpandState(element, expandStateAttrb));
     });
 }
